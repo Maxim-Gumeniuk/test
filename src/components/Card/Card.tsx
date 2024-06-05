6b9620e36884fa247c4d27b97e9bac3f2cf22fa0
maksym-debutify <maksym.humeniuk@debutify.com>
2024-06-05 15:42:21 +0300

    fixed mobile view

--- src/components/Card/Card.tsx
+++ src/components/Card/Card.tsx
@@ -5,8 +5,8 @@ interface Props {
     item: CardType;
 }
 const Card: FC<Props> = ({ item }) => (
-    <div className="bg-white rounded-lg w-[318px] md:w-[438px] flex h-[212px] md:h-[336px] mb-10">
-        <div className="flex flex-col gap-1  w-[120px] md:w-[180px] h-full">
+    <div className="bg-white rounded-lg w-[318px] md:w-[438px] flex h-[212px] md:h-[336px] mb-10 overflow-hidden">
+        <div className="flex flex-col gap-1 w-[120px] md:w-[180px] h-full">
             {item.images.map((img, index) => (
                 <img
                     alt={item.title}
@@ -22,7 +22,7 @@ const Card: FC<Props> = ({ item }) => (
             ))}
         </div>
 
-        <div className="px-2 pt-2 md:px-8 md:pt-10 flex flex-col gap-3 text-[#353844]">
+        <div className="px-2 pt-2 md:px-8 md:pt-10 flex flex-col gap-3 text-[#353844] min-h-full">
             <div className="font-bold text-[20px] md:text-[32px]">
                 {item.title}
             </div>
